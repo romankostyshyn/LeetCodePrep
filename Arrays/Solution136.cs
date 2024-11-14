3a76dc5a9ca5a08f400f076b771a8d61b8d30513
roman_kostyshyn <roman.kostyshyn98@gmail.com>
2024-11-14 23:11:37 +0100

    [ADD] Solution for task №136.
    #Branch: main

---
+++ Arrays/Solution136.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Arrays
+{
+    public class Solution136
+    {
+        public int SingleNumber(int[] nums)
+        {
+            int answer = 0;
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                answer = answer ^ nums[i];
+            }
+
+            return answer;
+        }
+    }
+}
