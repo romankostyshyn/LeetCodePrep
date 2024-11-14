76a84d5812eb6fc70c817b4baa5b3152d1b5733d
roman_kostyshyn <roman.kostyshyn98@gmail.com>
2024-11-14 18:07:20 +0100

    [ADD] Solution for task №88.
    #Branch: main

---
+++ Arrays/Solution88.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Arrays
+{
+    public class Solution88
+    {
+        public void Merge(int[] nums1, int m, int[] nums2, int n)
+        {
+            int x = m - 1;
+            int y = n - 1;
+            int z = nums1.Length - 1;
+
+            while (y >= 0)
+            {
+                if (x >= 0 && nums1[x] > nums2[y])
+                {
+                    nums1[z] = nums1[x];
+                    x--;
+                }
+                else
+                {
+                    nums1[z] = nums2[y];
+                    y--;
+                }
+                z--;
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -9,11 +9,13 @@ public class Program
         Solution35 solution35 = new Solution35();
         Solution27 solution27 = new Solution27();
         Solution66 solution66 = new Solution66();
+        Solution88 solution88 = new Solution88();
 
         //solution1.TwoSum([1, 2, 3], 3);
         //Console.WriteLine($"{solution26.RemoveDuplicates([1, 1, 2])}");
         //Console.WriteLine($"{solution35.SearchInsert([1, 3, 4, 5, 6, 7, 8], 2)}");
         //Console.WriteLine($"{solution27.RemoveElement([0, 1, 2, 2, 3, 0, 4, 2], 2)}");
-        solution66.PlusOne([8, 9, 9, 9]);
+        //solution66.PlusOne([8, 9, 9, 9]);
+        solution88.Merge([0], 0, [1], 1);
     }
 }
